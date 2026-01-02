7f9ceb21ee395b3eb46586875e3868393379f508
Daniel Castro <dcastro@ualberta.ca>
2026-01-02 00:42:28 -0700

    feat: added tab support to importer

--- src/kebihelp/config.py
+++ src/kebihelp/config.py
@@ -6,12 +6,14 @@ from prettytable import PrettyTable
 # import libs.Group as Group
 # import libs.Shortcut as Shortcut
 
-CONFIG_FILE = "~/.config/kebihelp.json"
+CONFIG_FILE = "~/.config/kebihelp/kebihelp.json"
+SOURCES_FILE = "~/.config/kebihelp/sources.json"
 
-class Config():
+
+class Config:
     def __init__(self):
         config_file = os.path.expanduser(CONFIG_FILE)
-        if (not os.path.isfile(config_file)):
+        if not os.path.isfile(config_file):
             self.config = self.default()
             self.save()
         with open(config_file) as f:
@@ -19,25 +21,26 @@ class Config():
 
     def default(self):
         return {
-            'Parameters': {
-                'cmd_focused_window': "hyprctl activewindow",
+            "Parameters": {
+                "cmd_focused_window": "hyprctl activewindow",
                 "importer": {
-                      "match_line": ".*::(.+?)::(.+?)::(.+?)::.*",
-                      "position_group": 1,
-                      "position_keybinding": 2,
-                      "position_label": 3
+                    "match_line": ".*::(.+?)::(.+?)::(.+?)::(.+?)::.*",
+                    "position_tab": 1,
+                    "position_group": 2,
+                    "position_keybinding": 3,
+                    "position_label": 4,
                 },
                 "keys": {
-                    "tab_previous": "Backtab", 
+                    "tab_previous": "Backtab",
                     "tab_next": "Tab",
                     "scroll_down": "Down",
                     "scroll_up": "Up",
-                    "quit": "Esc"
+                    "quit": "Esc",
                 },
                 "layout": {
-                    'n_columns': 3,
-                    'spacing': 3,
-                    'background_color': '#282C34',
+                    "n_columns": 3,
+                    "spacing": 3,
+                    "background_color": "#282C34",
                     "opacity": 0.95,
                     "tabs": {
                         "background_color": "#30343D",
@@ -45,82 +48,103 @@ class Config():
                         "color": "#FFFFFF",
                         "font": "Lato",
                         "font_size": 8,
-                        "padding": 0
+                        "padding": 0,
                     },
-                    'group_name': {
-                        'background_color': '#FF0000',
-                        'color': '#000000',
-                        'bold': True,
-                        'stretched': False,
+                    "group_name": {
+                        "background_color": "#FF0000",
+                        "color": "#000000",
+                        "bold": True,
+                        "stretched": False,
                         "padding": "0px 5px 0px 2px",
                         "font": "Lato",
-                        "font_size": 8
+                        "font_size": 8,
                     },
                     "shortcut": {
                         "label": {
-                          "color": "#EEEEEE",
-                          "bold": False,
-                          "font": "Lato",
-                          "font_size": 8
+                            "color": "#EEEEEE",
+                            "bold": False,
+                            "font": "Lato",
+                            "font_size": 8,
                         },
                         "value": {
-                          "color": "#EEEEEE",
-                          "bold": True,
-                          "font": "Lato",
-                          "font_size": 8
+                            "color": "#EEEEEE",
+                            "bold": True,
+                            "font": "Lato",
+                            "font_size": 8,
                         },
-                        "spacing": 5
+                        "spacing": 5,
                     },
-                }
+                },
             },
-            'Rules': {
-                'default': 'default'
-            },
-            'Keybindings': {}
+            "Rules": {"default": "default"},
+            "Keybindings": {},
         }
+
     def save(self):
         config_file = os.path.expanduser(CONFIG_FILE)
-        with open(config_file, 'w') as f:
+        with open(config_file, "w") as f:
             json.dump(self.config, f, indent=2)
 
     def add_keybinding(self, tab, group, keybinding, description):
-        print("Importing keybinding to tab {}: Group {} - {} - {}".format(tab, group, keybinding, description))
-        if not tab in self.config['Keybindings']:
-            self.config['Keybindings'][tab] = {
-                '_hidden': False,
+        print(
+            "Importing keybinding to tab {}: Group {} - {} - {}".format(
+                tab, group, keybinding, description
+            )
+        )
+        if not tab in self.config["Keybindings"]:
+            self.config["Keybindings"][tab] = {
+                "_hidden": False,
             }
 
-        if not group in self.config['Keybindings'][tab]:
-            self.config['Keybindings'][tab][group] = { '_hidden': False, 'keybindings': [] } 
-        
-        found = False
-        for idx, k in enumerate(self.config['Keybindings'][tab][group]['keybindings']):
-            if k['value'] == keybinding:
-                found = True
-                self.config['Keybindings'][tab][group]['keybindings'][idx]['label'] = description
-        if not found: 
-            self.config['Keybindings'][tab][group]['keybindings'].append({
-                'value': keybinding,
-                'label': description
-        })
+        if not group in self.config["Keybindings"][tab]:
+            self.config["Keybindings"][tab][group] = {
+                "_hidden": False,
+                "keybindings": [],
+            }
 
-    
+        found = False
+        for idx, k in enumerate(self.config["Keybindings"][tab][group]["keybindings"]):
+            if k["value"] == keybinding:
+                found = True
+                self.config["Keybindings"][tab][group]["keybindings"][idx]["label"] = (
+                    description
+                )
+        if not found:
+            self.config["Keybindings"][tab][group]["keybindings"].append(
+                {"value": keybinding, "label": description}
+            )
+
     def output_table(self, tab_selection):
-        table = PrettyTable(["\033[33mTab\033[0m", "\033[33mGroup\033[0m", "\033[33mKey\033[0m", "\033[33mLabel\033[0m", "\033[33mHidden\033[0m"])
-        for tab_name, tab_conf in self.config['Keybindings'].items():
+        table = PrettyTable(
+            [
+                "\033[33mTab\033[0m",
+                "\033[33mGroup\033[0m",
+                "\033[33mKey\033[0m",
+                "\033[33mLabel\033[0m",
+                "\033[33mHidden\033[0m",
+            ]
+        )
+        for tab_name, tab_conf in self.config["Keybindings"].items():
             if tab_selection != None and tab_selection != tab_name:
                 continue
             hidden = False
-            if '_hidden' in tab_conf and tab_conf['_hidden']:
-                hidden = True 
-            for group_name, group_conf in self.config['Keybindings'][tab_name].items():
-                if group_name == '_hidden':
+            if "_hidden" in tab_conf and tab_conf["_hidden"]:
+                hidden = True
+            for group_name, group_conf in self.config["Keybindings"][tab_name].items():
+                if group_name == "_hidden":
                     continue
-                if '_hidden' in group_conf and group_conf['_hidden']:
+                if "_hidden" in group_conf and group_conf["_hidden"]:
                     hidden = True
-                for keybinding in group_conf['keybindings']:
-                    if '_hidden' in keybinding and keybinding['_hidden']:
+                for keybinding in group_conf["keybindings"]:
+                    if "_hidden" in keybinding and keybinding["_hidden"]:
                         hidden = True
-                    table.add_row([tab_name, group_name, keybinding['value'], keybinding['label'], hidden])
+                    table.add_row(
+                        [
+                            tab_name,
+                            group_name,
+                            keybinding["value"],
+                            keybinding["label"],
+                            hidden,
+                        ]
+                    )
         print(table)
-
--- src/kebihelp/parsers.py
+++ src/kebihelp/parsers.py
@@ -1,4 +1,3 @@
-
 import os
 import re
 import sys
@@ -8,16 +7,23 @@ from kebihelp.config import Config
 from prettytable import PrettyTable
 import importlib
 
-class Parsers():
+
+class Parsers:
     def __init__(self, destination_tab, source):
         self.destination_tab = destination_tab
         self.source = source
         self.config = Config()
 
     def import_from_quickref(self):
-        req = urllib.request.Request(self.source, data=None, headers= {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+        req = urllib.request.Request(
+            self.source,
+            data=None,
+            headers={
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
+            },
+        )
         f = urllib.request.urlopen(req)
-        html = f.read().decode('utf-8')
+        html = f.read().decode("utf-8")
         sections = html.split('<div class="h3-wrap">')
         sections.pop(0)
 
@@ -26,39 +32,64 @@ class Parsers():
             title = z.group(1)
             trs = re.findall(r"<tr>(.+?)</tr>", section, re.MULTILINE + re.DOTALL)
             for tr in trs:
-                tds = re.findall(r"<td>(.+?)</td>", tr ,re.MULTILINE + re.DOTALL)
-                if (len(tds) > 1):
+                tds = re.findall(r"<td>(.+?)</td>", tr, re.MULTILINE + re.DOTALL)
+                if len(tds) > 1:
                     label = tds[1]
-                    value = tds[0].replace("</code> <code>", "+").replace("</code>", "").replace('<code>', "")
-                    self.config.add_keybinding(self.destination_tab, section, value, label)
+                    value = (
+                        tds[0]
+                        .replace("</code> <code>", "+")
+                        .replace("</code>", "")
+                        .replace("<code>", "")
+                    )
+                    self.config.add_keybinding(
+                        self.destination_tab, section, value, label
+                    )
 
     def import_from_directory(self):
         # Format: KBH=tab,value,description
-        files = glob.glob(self.source+"/**/*", recursive=True)
+        files = glob.glob(self.source + "/**/*", recursive=True)
         if not files:
             print("No files found")
             sys.exit(1)
-        regexp = self.config.config['Parameters']['importer']['match_line']
+        regexp = self.config.config["Parameters"]["importer"]["match_line"]
         for file in files:
             # print("Importing from file "+file)
             if os.path.isfile(file):
-                with open(file, 'r') as file:
+                with open(file, "r") as file:
                     lines = file.readlines()
                     for line in lines:
                         line = line.strip()
                         res = re.match(regexp, line, re.IGNORECASE)
                         if res:
-                            group = res.group(self.config.config['Parameters']['importer']['position_group'])
-                            keybind = res.group(self.config.config['Parameters']['importer']['position_keybinding'])
-                            label = res.group(self.config.config['Parameters']['importer']['position_label'])
-                            self.config.add_keybinding(self.destination_tab, group, keybind, label)
-
+                            tab = self.destination_tab
+                            if self.config.config["Parameters"]["importer"].get(
+                                "position_tab"
+                            ):
+                                tab = res.group(
+                                    self.config.config["Parameters"]["importer"][
+                                        "position_tab"
+                                    ]
+                                )
+                            group = res.group(
+                                self.config.config["Parameters"]["importer"][
+                                    "position_group"
+                                ]
+                            )
+                            keybind = res.group(
+                                self.config.config["Parameters"]["importer"][
+                                    "position_keybinding"
+                                ]
+                            )
+                            label = res.group(
+                                self.config.config["Parameters"]["importer"][
+                                    "position_label"
+                                ]
+                            )
+                            self.config.add_keybinding(tab, group, keybind, label)
 
     def import_bindings(self):
-        if 'quickref.me' in self.source:
+        if "quickref.me" in self.source:
             self.import_from_quickref()
         else:
             self.import_from_directory()
         self.config.save()
-
-
